ec726e968d7798ac96ad8eea44dd27de09a360ca
captainsalt <dontbothertheking@gmail.com>
2017-11-29 20:59:53 -0600

    initial

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: [
+            "./client/index.js"
+        ]
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "client/dist")
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [
+                'babel-loader',
+            ],
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./client/public/index.html"
+        }),
+        new CleanWebpackPlugin(["dist"], {
+            root: path.join(__dirname, "client"),
+            verbose: true
+        })
+    ]
+}
