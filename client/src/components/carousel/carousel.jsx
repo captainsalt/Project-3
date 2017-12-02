36d0e9836c7706ee3f34a2d7da94f34f3dedbf5d
Grant Chan <grant.r.chan@gmail.com>
2017-12-02 14:10:58 -0600

    Main page skeleton completed

--- client/src/components/carousel/carousel.jsx
+++ client/src/components/carousel/carousel.jsx
@@ -5,15 +5,16 @@ export default class CarouselBS extends Component {
     render() {
         return (
             <Carousel>
+                {/* <div id="csl"> */}
                 <Carousel.Item>
-                    <img width={700} height={400} alt="700x400" src="https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/2:1/w_1260%2Ch_630/the-ultimate-hamburger.jpg" className="csl"/>
+                    <img width={500} height={400} className="csl" alt="500x400" src="https://www.reviewjournal.com/wp-content/uploads/2017/10/9051518_web1_art-in-the-park_100817-_gb_016.jpg"/>
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img width={700} height={400} alt="700x400" src="https://www.kingsford.com/wp-content/uploads/2014/11/kfd-howtohamburger-Burgers_5_0391-1024x621.jpg" className="csl"/>
+                    <img width={500} height={400} className="csl" alt="500x400" src="https://i.pinimg.com/originals/f7/81/f8/f781f84a37411064e0421cd74c5781c4.jpg"/>
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -26,6 +27,7 @@ export default class CarouselBS extends Component {
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
                 </Carousel.Item> */}
+                {/* </div> */}
             </Carousel>
         )
     }
