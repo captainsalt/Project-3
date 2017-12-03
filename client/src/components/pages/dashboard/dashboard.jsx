9fe4150b22dce4a66820ba2a8f9295d999f64d74
captainsalt <dontbothertheking@gmail.com>
2017-12-03 16:33:54 -0600

    Basic dashboard

---
+++ client/src/components/pages/dashboard/dashboard.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Image, Row, Col, Panel, ListGroup, ListGroupItem } from "react-bootstrap";
+import testImages from "../testimages.json";
+import "./dashboard.css"
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                {/* Logo row*/}
+                <Row>
+                    <Col md={12}>
+                        Logo
+                    </Col>
+                </Row>
+
+                {/* Portrait row*/}
+                <Row>
+                    {/* Portrait */}
+                    <Col xs={2} md={5}>
+                        <Image
+                            itemID="portrait"
+                            src={testImages[0].imgUrl}
+                            alt={testImages[0].alttext}
+                            height={500}
+                            width={500}
+                            responsive
+                        />
+                    </Col>
+
+                    {/* informarion*/}
+                    <Col md={7}>
+                        <h2>Title</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor ante ut dui egestas egestas. Ut erat sem, dapibus in leo vitae, rhoncus accumsan massa. Nam quis sem sagittis, condimentum nibh vel, tempus magna. Sed interdum, purus vel venenatis molestie, nibh massa aliquet libero, vitae finibus leo nisl sit amet nunc. Quisque varius elementum neque, vitae faucibus nibh suscipit non. Sed arcu velit, rhoncus eu viverra id, condimentum eu mi. Nunc porta, urna vitae accumsan semper, ex ex auctor nulla, et tincidunt dolor ante quis magna. Vestibulum at ligula non orci laoreet condimentum. Fusce sed tempus ante. Morbi sollicitudin erat non nisl lobortis, id molestie justo vulputate. Praesent nec scelerisque quam, non tristique nunc. Morbi rhoncus, mi eu rhoncus commodo, ligula odio consequat ante, ut tempus nunc quam nec lectus. Nunc porttitor, arcu quis interdum gravida, augue felis laoreet sapien, vitae consequat dui ex sit amet nibh. Mauris vehicula, est et eleifend vehicula, turpis ligula ullamcorper risus, non iaculis odio velit sed odio. Integer vitae lobortis est. Vivamus varius eu libero eget tincidunt.
+                        </p>
+
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
