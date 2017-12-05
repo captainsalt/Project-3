4349cae4b72a465cb2c3c7f45416902d57e1902f
Grant Chan <grant.r.chan@gmail.com>
2017-12-04 19:36:47 -0600

    added display items to discover

--- client/src/components/pages/discover/discover.jsx
+++ client/src/components/pages/discover/discover.jsx
@@ -25,7 +25,7 @@ export default class Discover extends Component {
                     </Col>
                     <Col xs={12} md={9}>
                         <Panel className="cat-user">
-                            <Col xs={3} md={3}>
+                            <Col xs={12} md={3}>
                                 <Image
                                     className="feat-user"
                                     src={testImages[0].imgUrl}
@@ -33,9 +33,16 @@ export default class Discover extends Component {
                                     circle
                                 />
                             </Col>
-                            <Col xs={9} md={9}>
+                            <Col xs={12} md={9}>
+                            <Row className="user-pane">
                                 <h3>Annie May</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor ante ut dui egestas egestas. Ut erat sem, dapibus in leo vitae, rhoncus accumsan massa. Nam quis sem sagittis, condimentum nibh vel, tempus magna. Sed interdum, purus vel venenatis molestie, nibh massa aliquet libero, vitae finibus leo nisl sit amet nunc. Quisque varius elementum neque, vitae faucibus nibh suscipit non. Sed arcu velit, rhoncus eu viverra id, condimentum eu mi. Nunc porta, urna vitae accumsan semper, ex ex auctor nulla, et tincidunt dolor ante quis magna. Vestibulum at ligula non orci laoreet condimentum. Fusce sed tempus ante. Morbi sollicitudin erat non nisl lobortis, id molestie justo vulputate. Praesent nec scelerisque quam, non tristique nunc. Morbi rhoncus, mi eu rhoncus commodo, ligula odio consequat ante, ut tempus nunc quam nec lectus. Nunc porttitor, arcu quis interdum gravida, augue felis laoreet sapien, vitae consequat dui ex sit amet nibh. Mauris vehicula, est et eleifend vehicula, turpis ligula ullamcorper risus, non iaculis odio velit sed odio. Integer vitae lobortis est. Vivamus varius eu libero eget tincidunt.</p>
+                                <p>Graffiti Artist. I also craft shoes.</p>
+                            </Row>
+                            <Row className="disp-work">
+                                <Image className="samp-work" src={testImages[0].works[0]} circle/>
+                                    <Image className="samp-work" src={testImages[0].works[1]} circle />
+                                    <Image className="samp-work" src={testImages[0].works[2]} circle />
+                            </Row>
                             </Col>
                         </Panel>
                         <Panel className="cat-user">
